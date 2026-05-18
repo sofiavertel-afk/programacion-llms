e509a0b29b039f45cda5f78d8a293b953c1943af
sofiavertel-afk <sofia.vertel@udea.edu.co>
2026-05-18 14:23:23 -0500

    Create answer-0618.py

---
+++ myanswers/answer-0618.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+def detectar_outliers_iqr(df, col):
+    df = df.copy()
+
+    Q1 = df[col].quantile(0.25)
+    Q3 = df[col].quantile(0.75)
+    IQR = Q3 - Q1
+
+    lower = Q1 - 1.5 * IQR
+    upper = Q3 + 1.5 * IQR
+
+    df["es_outlier"] = (df[col] < lower) | (df[col] > upper)
+
+    return df
